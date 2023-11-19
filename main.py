1e880bcaf91df66823eb9a895af7381d90803248
AngryRoomba <epayyz@gmail.com>
2023-11-19 14:20:17 -0500

    Created initial sinewave program

---
+++ main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plot
+import numpy as np
+import math
+
+time = np.arange(0, 2.1*math.pi, 0.1)
+amp = np.sin(time)
+plot.plot(time, amp)
+plot.title("Sine Wave")
+plot.xlabel("Time")
+plot.ylabel("Amplitude")
+plot.axhline(y=0, color='k')
+plot.show()
