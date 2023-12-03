a4cb1ad5e9ae72c41afeae12cc50a4a80c7f9d3f
MightBeRaptor <huntersecondemail@gmail.com>
2023-12-03 14:58:55 -0500

    changed from_mp3 to from_file

--- file_conversion.py
+++ file_conversion.py
@@ -3,5 +3,5 @@ from pydub import AudioSegment
 
 file_extension = os.path.splitext("recording.mp3")[1]
 if file_extension.lower() == ".mp3":
-    sound = AudioSegment.from_mp3("recording.mp3")
+    sound = AudioSegment.from_file("recording.mp3")
     sound.export("recording.wav", format="wav")
