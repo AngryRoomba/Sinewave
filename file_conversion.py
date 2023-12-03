ce9532fe7ae6fe0da1b1f9f4f851d6c52426c0c1
MightBeRaptor <huntersecondemail@gmail.com>
2023-12-03 16:19:52 -0500

    changes audio to mono if it is in stereo

--- file_conversion.py
+++ file_conversion.py
@@ -1,4 +1,5 @@
 import os
+import pydub.utils
 from pydub import AudioSegment
 
 file_extension = os.path.splitext("recording.mp3")[1]
@@ -6,7 +7,8 @@ if file_extension.lower() == ".mp3":
     sound = AudioSegment.from_file("recording.mp3")
     sound.export("recording.wav", format="wav")
 raw_audio = AudioSegment.from_file("recording.wav", format="wav")
-print(raw_audio.channels)
-if raw_audio.channels >= 2:
-    raw_audio.set_channels(1)
-    print(raw_audio.channels)
+if raw_audio.channels != 1:
+    raw_audio = raw_audio.set_channels(1)
+    raw_audio.export("recording_mono.wav", format="wav")
+else:
+    raw_audio.export("recording_mono.wav", format="wav")
