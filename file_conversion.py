15e4047c222cdda461a616f87af5eb47aacb298f
MightBeRaptor <huntersecondemail@gmail.com>
2023-12-05 10:05:28 -0500

    test commit & push and saved mono audio

--- file_conversion.py
+++ file_conversion.py
@@ -2,6 +2,7 @@ import os
 import pydub.utils
 from pydub import AudioSegment
 
+
 file_extension = os.path.splitext("recording.mp3")[1]
 if file_extension.lower() == ".mp3":
     sound = AudioSegment.from_file("recording.mp3")
@@ -12,4 +13,4 @@ if raw_audio.channels != 1:
     raw_audio.export("recording_mono.wav", format="wav")
 else:
     raw_audio.export("recording_mono.wav", format="wav")
-print(pydub.utils.mediainfo("recording_mono.wav"))
+raw_audio_mono = AudioSegment.from_file("recording_mono.wav", format="wav")
