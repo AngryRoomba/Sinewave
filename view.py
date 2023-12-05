202f917702849f60a620610e8c5de208fdd4e65f
AngryRoomba <epayyz@gmail.com>
2023-12-05 11:22:45 -0500

    converted view into class for implementation within controller.py, created controller.py, changed names of files to align with MVC model

---
+++ view.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import numpy as np
+from pydub import AudioSegment
+import scipy.io
+from scipy.io import wavfile
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class View:
+    def __init__(self):
+        root = Tk()
+        root.title("Audiowave display")
+        root.configure(background="grey")
+        root.minsize(400, 400)
+        root.geometry('860x640+360+80')
+
+        self.request = Label(root, text='Please choose an audio file', bg='white', fg='black')
+        self.request.pack(side="top", pady=(5,0))
+        self.reqButton = Button(root, text='choose an audio file',command=self.open_file)
+        self.reqButton.pack(side='top', pady=(5,0))
+
+        self.checkButton = Button(root, text='Plot the given data', command=self.plotData)
+
+        self.dataDisplay = Label(root, text="Place holder for echo data", bg='white', fg='black')
+
+        self.fig = Figure(figsize= (5,5), dpi=100)
+        self.graph = FigureCanvasTkAgg(self.fig, master=root)
+        root.mainloop()
+
+
+    def open_file(self):
+        filepath = filedialog.askopenfilename(title='choose an audio file')
+        if ('.wav' in filepath) or ('.mp3' in filepath):
+            self.request['text']= filepath
+            self.checkButton.pack(side='top', pady=(5,0))
+            self.dataDisplay.pack_forget()
+            self.graph.get_tk_widget().pack_forget()
+        else:
+            self.request['text']= "ERROR: File must be .wav or .mp3!"
+            self.checkButton.pack_forget()
+            self.dataDisplay.pack_forget()
+            self.graph.get_tk_widget().pack_forget()
+            return
+        filename = os.path.basename(filepath)
+        self.request['text'] = filepath
+        if '.mp3' in filename:
+            self.request['text'] = 'finding...'
+            sound = AudioSegment.from_file(filepath)
+            self.request['text'] = 'converting...'
+            sound.export('file.wav', format='wav')
+            self.request['text'] = 'converted!'
+
+
+
+    def plotData(self):
+        samplerate, data = wavfile.read(self.request['text'])
+        length = data.shape[0] / samplerate
+        time = np.linspace(0., length, data.shape[0])
+        plotter = self.fig.add_subplot(111)
+        plotter.plot(time, data[:])
+        self.graph.draw()
+        self.graph.get_tk_widget().pack(side='left', pady=(5,0))
+        self.dataDisplay.pack(side='right')
