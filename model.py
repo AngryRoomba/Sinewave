b689ea92183310f0f5a9682e1657dcd791812a4c
MightBeRaptor <huntersecondemail@gmail.com>
2023-12-07 13:44:27 -0500

    line 16, sound is from file

--- model.py
+++ model.py
@@ -13,15 +13,12 @@ class Model:
     def format_conversion(self):
         file_extension = os.path.splitext(self.file)[1]
         if file_extension.lower() == ".mp3":
-            sound = AudioSegment.from_file("recording.mp3")
+            sound = AudioSegment.from_file(self.file)
             sound.export("recording.wav", format="wav")
         self.raw_audio = AudioSegment.from_file("recording.wav", format="wav")
-
 
     def audio_to_mono(self):
         if self.raw_audio.channels != 1:
             self.raw_audio = self.raw_audio.set_channels(1)
-            self.raw_audio.export("recording_mono.wav", format="wav")
-        else:
-            self.raw_audio.export("recording_mono.wav", format="wav")
-        self.raw_audio_mono = AudioSegment.from_file("recording_mono.wav", format="wav")
+            self.raw_audio.export("recording.wav", format="wav")
+        self.raw_audio_mono = AudioSegment.from_file("recording.wav", format="wav")
