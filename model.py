92815b59768aa3b054a1cc22acdde9dc2e9ec7b0
MightBeRaptor <huntersecondemail@gmail.com>
2023-12-08 13:35:30 -0500

    METADATA IS GONE

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import os
-import pydub.utils
+import ffmpeg
 from pydub import AudioSegment
 
 
@@ -11,14 +11,18 @@ class Model:
         self.raw_audio_mono = None
 
     def format_conversion(self):
-        file_extension = os.path.splitext(self.file)[1]
-        if file_extension.lower() == ".mp3":
-            sound = AudioSegment.from_file(self.file)
-            sound.export("recording.wav", format="wav")
-        self.raw_audio = AudioSegment.from_file("recording.wav", format="wav")
+        sound = AudioSegment.from_file(self.file)
+        sound.export("recording.wav", format="wav")
+        self.file = 'recording.wav'
+        self.raw_audio = AudioSegment.from_file(self.file, format="wav")
 
     def audio_to_mono(self):
         if self.raw_audio.channels != 1:
             self.raw_audio = self.raw_audio.set_channels(1)
             self.raw_audio.export("recording.wav", format="wav")
         self.raw_audio_mono = AudioSegment.from_file("recording.wav", format="wav")
+
+
+audio = Model("bruh.wav")
+print(ffmpeg.probe("bruh.wav"))
+audio.format_conversion()
