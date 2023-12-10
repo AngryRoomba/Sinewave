cef2a1ceec81e43754adaafd4093096b3ddb889b
Maxime-Weeks <150705736+Maxime-Weeks@users.noreply.github.com>
2023-12-09 21:26:21 -0500

    Update view.py
    Removed debugging code and added comments

--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+# Imports
 from tkinter import *
 from tkinter import filedialog
 import os
@@ -10,38 +11,47 @@ from scipy.io import wavfile
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from controller import Controller
 
-
+# Class definition
 class View:
+    # Creates all elements of the GUI
     def __init__(self):
+        # Creates the popup GUI
         root = Tk()
         root.title("Audiowave display")
         root.configure(background="grey")
         root.minsize(400, 400)
         root.geometry('980x760+300+20')
 
+        # Initialises the controller to allow model-view communication
         self.controller = Controller()
+        
+        # Creates the frame in which all elements will be arranged
         self.framebutton = Frame(root, bg='grey')
 
+        # Creates the upload button and prompt and attaches them to the frame
         self.request = Label(root, text='Please choose an audio file', bg='white', fg='black')
         self.request.pack(side="top", pady=(5, 0))
         self.reqButton = Button(root, text='Select Audio File', command=self.open_file)
         self.reqButton.pack(side='top', pady=(5, 0))
 
+        # Creates the various graph buttons and puts them in the frame
         self.checkButton = Button(master=self.framebutton, text='Plot RT60s', command=self.plotRT60s)
         self.waveButton = Button(master=self.framebutton, text='Show the Waveform', command=self.plotWaveform)
         self.spectButton = Button(master=self.framebutton, text='Show the Spectrogram', command=self.plotSpectrogram)
         self.totalButton = Button(root, text='Display all RT60s at once', command=self.plotAllRT60s)
 
-        self.time = Label(root, text='Time display', bg='white', fg='black')
-
+        # Creates the text box for additional information and binds it to the frame
         self.dataDisplay = Label(root, text="Place holder for echo data", bg='white', fg='black')
 
+        # Initialises a counter for the cycling RT60 graph button and the button itself
         self.RT60counter = 0
         self.cycleButton = Button(root, text='Show next RT60 graph', command=self.cycleRT60)
 
+        # Creates frames to contain graphs
         self.frametop = Frame(root)
         self.framebottom = Frame(root)
 
+        # Initialises figures for each graph and binds the appropriate graph to them.
         self.fig = Figure(figsize=(5, 5), dpi=100)
         self.dBfig = Figure(figsize=(5, 5), dpi=100)
         self.specfig = Figure(figsize=(5, 5), dpi=100)
@@ -55,12 +65,19 @@ class View:
         self.highGraph = FigureCanvasTkAgg(self.highfig, master=self.framebottom)
         self.totalgraph = FigureCanvasTkAgg(self.totalfig, master=root)
 
+        # Makes the window start working
         root.mainloop()
 
     def open_file(self):
+        # Creates the upload popup using the default file management application
         filepath = filedialog.askopenfilename(title='Select Audio File')
+        
+        # Gets the file extention by cutting the file path
         ext = os.path.splitext(filepath)[-1].lower()
+
+        # Checks if the file is .wav or .mp3
         if (ext == ".wav") or (ext == ".mp3"):
+            # If it is make the graph buttons visible and removes all curently visible graphs
             self.request['text'] = filepath
             self.framebutton.pack(side='top', pady=(5,0))
             self.checkButton.pack(side='left')
@@ -72,6 +89,8 @@ class View:
             self.unpackGraphs()
             self.frametop.pack_forget()
             self.framebottom.pack_forget()
+        
+        # If the File is not .wav or .mp3 throw an error and reset all graphs
         else:
             self.request['text'] = "ERROR: File must be .wav or .mp3!"
             self.checkButton.pack_forget()
@@ -82,17 +101,29 @@ class View:
             self.framebutton.pack_forget()
             self.totalButton.pack_forget()
             return
+
+        # Converts .mp3 files to .wav files
         self.controller.convert(self.request['text'])
+        
+        # Creates but does not show all the graphs
         self.plotData()
 
     def plotData(self):
+        # Gets all of the values from the math function
         t, DbData, iMax, i5, i25, file, rt60, highest = self.controller.math(1000)
         tLow, dBDataLow, iMaxLow, i5Low, i25Low, file, rt60Low, highestL = self.controller.math(250)
         tHigh, dBDataHigh, iMaxHigh, i5High, i25High, file, rt60High, highestH = self.controller.math(5000)
+        
+        # Gets the samplerate and data from the audio file
         samplerate, data = wavfile.read(file)
+        
+        # Determines the width of the graph
         length = data.shape[0] / samplerate
+        
+        # Determines what time the graph spans
         time = np.linspace(0., length, data.shape[0])
 
+        # Plots the medium RT60 graph
         dBplotter = self.dBfig.add_subplot(111)
         dBplotter.plot(t, DbData, linewidth=1, alpha=0.7, color='#004bc6')
         self.dBfig.supxlabel("Time (s)")
@@ -102,6 +133,7 @@ class View:
         dBplotter.plot(t[i5], DbData[i5], 'yo')
         dBplotter.plot(t[i25], DbData[i25], 'ro')
 
+        # Plots the low RT60 graph
         Lowplotter = self.lowfig.add_subplot(111)
         Lowplotter.plot(tLow, dBDataLow, linewidth=1, alpha=0.7, color='#004bc6')
         self.lowfig.supxlabel("Time (s)")
@@ -111,6 +143,7 @@ class View:
         Lowplotter.plot(t[i5Low], dBDataLow[i5Low], 'yo')
         Lowplotter.plot(t[i25Low], dBDataLow[i25Low], 'ro')
 
+        # Plots the high RT60 graph
         Highplotter = self.highfig.add_subplot(111)
         Highplotter.plot(tHigh, dBDataHigh, linewidth=1, alpha=0.7, color='#004bc6')
         self.highfig.supxlabel("Time (s)")
@@ -120,18 +153,21 @@ class View:
         Highplotter.plot(t[i5High], dBDataHigh[i5High], 'yo')
         Highplotter.plot(t[i25High], dBDataHigh[i25High], 'ro')
 
+        # Plots the spectrogram
         self.specfig.suptitle("Spectrogram")
         specPlotter = self.specfig.add_subplot(111)
         spec, fr, ti, im = specPlotter.specgram(data, Fs=samplerate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
         cbar = self.specfig.colorbar(im)
         cbar.set_label('Intensity (dB)')
 
+        # Plots the waveform
         self.fig.suptitle("Waveform")
         self.fig.supxlabel('Time (s)')
         self.fig.supylabel('Frequency (Hz)')
         plotter = self.fig.add_subplot(111)
         plotter.plot(time, data[:])
 
+        # Plots the combined RT60 graph
         self.totalfig.suptitle("All RT60s")
         self.totalfig.supylabel("Power (dB)")
         self.totalfig.supxlabel("Time (s)")
@@ -150,40 +186,38 @@ class View:
         totalplotter.plot(t[i25], DbData[i25], 'ro', label="_")
         self.totalfig.legend()
 
+        # Creates the additional information text using data gathered by the math dunction
         self.dataDisplay['text']= 'The file is ' + str(round(self.controller.model.time, 3)) + ' seconds long, the frequency of highest amplitude is '+ str(highest) + 'Hz and the average RT60s are:\n LOW: '+ str(round(rt60Low-0.5, 3)) + ' MEDIUM: ' + str(round(rt60-.5, 3)) + ' HIGH: '+ str(round(rt60High-0.5, 3))
         self.dataDisplay.pack(side='bottom')
-        #self.frametop.pack(side='top')
-        #self.framebottom.pack(side='bottom')
 
-        # self.dataDisplay.pack(side='right')
-        #self.dBgraph.draw()
-        #self.dBgraph.get_tk_widget().pack(side='left', padx=(3, 3), pady=(5, 0), anchor='nw', expand=True)
-        #self.lowGraph.draw()
-        #self.lowGraph.get_tk_widget().pack(side='left', anchor='sw', expand=True)
-        #self.highGraph.draw()
-        #self.highGraph.get_tk_widget().pack(side='bottom', anchor='sw', expand=True)
-
+    
     def plotSpectrogram(self):
+        # Deletes any previous graph
         self.unpackGraphs()
 
+        # Plots the spectrogram
         self.frametop.pack(pady=(20,0))
         self.specGraph.draw()
         self.specGraph.get_tk_widget().pack(side='left', pady=(5, 0), anchor='nw', expand=True)
 
     def plotWaveform(self):
+        # Deletes any previous graph
         self.unpackGraphs()
 
+        # Plots the waveform
         self.frametop.pack(pady=(20,0))
-        #self.framebottom.pack(side='bottom')
         self.graph.draw()
         self.graph.get_tk_widget().pack(side='left', padx=(5, 0), pady=(5, 0), anchor='nw', expand=True)
 
     def plotRT60s(self):
+        # Deletes any previous graph
         self.unpackGraphs()
 
+        # Ensures the cycle button is visible
         self.cycleButton.pack(pady=(20, 0))
-
         self.framebottom.pack(pady=(20,0))
+
+        # Checks which graph to display by checking the value of the counter
         if self.RT60counter == 0:
             self.lowGraph.draw()
             self.lowGraph.get_tk_widget().pack(side='left', anchor='sw', expand=True)
@@ -195,19 +229,27 @@ class View:
             self.highGraph.get_tk_widget().pack(side='bottom', anchor='sw', expand=True)
 
     def cycleRT60(self):
+        # Increments the counter
         self.RT60counter += 1
+
+        # Ensures the counter cannot exceed 2
         if self.RT60counter > 2:
             self.RT60counter = 0
+        
+        # Plots the graph
         self.plotRT60s()
 
     def plotAllRT60s(self):
+        # Deletes any previous graph
         self.unpackGraphs()
 
+        # Plots the combined RT60 graph
         self.totalgraph.draw()
         self.totalgraph.get_tk_widget().pack(pady=(20,0))
 
 
     def unpackGraphs(self):
+        # Hides all graphs
         self.graph.get_tk_widget().pack_forget()
         self.dBgraph.get_tk_widget().pack_forget()
         self.specGraph.get_tk_widget().pack_forget()
@@ -221,11 +263,12 @@ class View:
 
 
     def getCurretFile(self):
+        # Returns the location of the selected file
         if self.request['text'] == "ERROR: File must be .wav or .mp3!" or self.request[
             'text'] == 'choose an audio file':
             return 0
         else:
             return self.request['text']
 
-
+# Runs the program
 view = View()
