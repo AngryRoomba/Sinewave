0afbfded84c903b9f8f3ea7b0e29af87d2af602f
Maxime-Weeks <150705736+Maxime-Weeks@users.noreply.github.com>
2023-12-09 20:51:52 -0500

    Update controller.py
    Removed debugging code and added comments

--- controller.py
+++ controller.py
@@ -1,17 +1,19 @@
+# Imports
 from model import Model
 
-
+# Class definition
 class Controller:
-
+    # When crated create a blank model
     def __init__(self):
         self.model = Model("")
 
+    # Converts the file to .wav and mono audio by calling related functions
     def convert(self, file):
-        # print('converting')
         self.model.file = file
         self.model.format_conversion()
         self.model.audio_to_mono()
         return
 
+    # Returns all calculations associated with the audio file
     def math(self, targetFreq):
         return self.model.math(targetFreq)
